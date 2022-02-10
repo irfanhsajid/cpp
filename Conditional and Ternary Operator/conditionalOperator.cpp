ff9b0eb6d937d437c86fc8be5aa5d0b6f8c8115f
irfanhsajid <irfanhsajid@gmail.com>
2022-02-10 16:30:55 +0600

    upto loop

---
+++ Conditional and Ternary Operator/conditionalOperator.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+
+     int num1, num2;
+     num1=20;
+     num2= 30;
+
+     int max = (num1>num2)? num1 : num2;
+
+     cout << max;
+
+}
---
+++ Conditional and Ternary Operator/ternaryEvenOdd.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+
+int num;
+
+cout << "Enter any Number : ";
+
+cin >> num;
+
+(num%2==0) ? cout <<num << " is even" : cout <<num << " is odd";
+
+
+}
